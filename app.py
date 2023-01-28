0461994eb2349d6ac32eb2d6729fa164005ffad9
Karthik Putchala <54702629+Karthikputchala@users.noreply.github.com>
2023-01-28 17:28:44 +1100

    -c

--- app.py
+++ app.py
@@ -4,13 +4,16 @@ import streamlit as st
 
 snipped_folder_path = r"C:\Users\personal\Projects\findme\Data\snipped_clips"
 
-# Introdution of the project
-about()
-# Take the User input
-video_loc, image_loc= input()
-# If the "Generate button is pressed"
-if st.button('Generate'):
-    # Runs the main function
-    output, time_stamps, list_of_extractedFilePaths, labels = main(image_loc,video_loc,snipped_folder_path)
-    # Displays the results to the user
-    display_result(output, time_stamps, labels)
+def run():
+    # Introdution of the project
+    about()
+    # Take the User input
+    video_loc, image_loc= input()
+    # If the "Generate button is pressed"
+    if st.button('Generate'):
+        # Runs the main function
+        output, time_stamps, list_of_extractedFilePaths, labels = main(image_loc,video_loc,snipped_folder_path)
+        # Displays the results to the user
+        display_result(output, time_stamps, labels)
+
+run()
